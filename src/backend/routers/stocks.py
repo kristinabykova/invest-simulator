e69fce71e0a2f69249b04ffea97b620077f40b37
kristinabykova <kristina.bykova.03@list.ru>
2026-03-04 12:11:36 +0300

    add cash

--- src/backend/routers/stocks.py
+++ src/backend/routers/stocks.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, HTTPException, status
 from services.moex import get_stock_candles, get_stock_lotsize
 from services.stocks import list_of_stocks, is_supported_ticker
 from datetime import date, timedelta
+from services.cache_services import redis_client
+import json
 
 router = APIRouter(prefix="/stocks", tags=["Stocks"])
-
 
 @router.get("/")
 def get_stocks():
@@ -12,13 +13,13 @@ def get_stocks():
 
 @router.get("/{ticker}/lotsize")
 def get_lotsize(ticker: str):
-    lotsize = get_stock_lotsize(ticker)
-
-    if lotsize == None:
+    if not is_supported_ticker(ticker):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail={"code": "TICKER_NOT_FOUND", "msg": f"Unknown ticker: {ticker}"}
         )
+    lotsize = get_stock_lotsize(ticker)
+
     return {"ticker":ticker, "lotsize": lotsize}
 
 @router.get("/{ticker}/history")
@@ -29,10 +30,24 @@ def stock_history(ticker: str, days: int = 3):
             detail={"code": "TICKER_NOT_FOUND", "msg": f"Unknown ticker: {ticker}"}
         )
     
+    cache_key = f"stock_history:{ticker}:{days}"
+    cached_data = redis_client.get(cache_key)
+    if cached_data:
+        return json.loads(cached_data)
+    
     today = date.today()
     date_from = today - timedelta(days=days)
-    return get_stock_candles(
+    result = get_stock_candles(
         ticker=ticker,
         date_from=date_from.isoformat(),
         date_to=today.isoformat()
     )
+
+    redis_client.setex(
+        cache_key,
+        60,  
+        json.dumps(result)
+    )
+
+    return result
+
